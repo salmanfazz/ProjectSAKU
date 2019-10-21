a16b59de885a9e45029207140216238f02e396ab
devptsai <devptsai@gmail.com>
2019-10-21 16:48:40 +0700

    Notification

--- app/src/main/java/com/example/mockupsai/MessageDetail.java
+++ app/src/main/java/com/example/mockupsai/MessageDetail.java
@@ -1,13 +1,17 @@
 package com.example.mockupsai;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MessageDetail extends Fragment implements View.OnClickListener{
 
@@ -18,7 +22,21 @@ public class MessageDetail extends Fragment implements View.OnClickListener{
     }
 
     @Override
-    public void onClick(View view) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        RelativeLayout back = (RelativeLayout) getView().findViewById(R.id.back);
+        back.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        Fragment fragment = null;
+
+        switch (view.getId()) {
+            case R.id.back:
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new Messages());
+                fragmentTransaction.commit();
+                break;
+        }
     }
 }
