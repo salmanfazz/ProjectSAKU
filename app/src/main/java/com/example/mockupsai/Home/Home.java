3cdf7b3d658d325f47ffeb830ca9e41bc1527e8e
devptsai <devptsai@gmail.com>
2019-10-24 11:26:45 +0700

    Final

--- app/src/main/java/com/example/mockupsai/Home/Home.java
+++ app/src/main/java/com/example/mockupsai/Home/Home.java
@@ -1,21 +1,59 @@
 package com.example.mockupsai.Home;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.mockupsai.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Home extends Fragment {
+    RecyclerView recyclerView;
+    List<Homes> homesList;
+    HomeRecyclerViewHorizontalAdapter homeRecyclerViewHorizontalAdapter;
+
+    String[] title = {"Matematika", "Bahasa Inggris", "Bahasa Jepang"};
+    String[] date = {"10 Oct 2019", "12 Oct 2019", "14 Oct 2019"};
+    String[] time = {"07:00", "10:00", "14:00"};
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
-        return inflater.inflate(R.layout.activity_home, null);
+        LayoutInflater If = getActivity().getLayoutInflater();
+        View view = If.inflate(R.layout.activity_home, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        recyclerView = (RecyclerView) getView().findViewById(R.id.listToDo);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        if (recyclerView != null) {
+            recyclerView.setHasFixedSize(true);
+        }
+
+        homesList = new ArrayList<>();
+
+        for (int i = 0; i < title.length; i++) {
+            Homes homes = new Homes(title[i], date[i], time[i]);
+            homesList.add(homes);
+        }
+
+        homeRecyclerViewHorizontalAdapter = new HomeRecyclerViewHorizontalAdapter(homesList);
+        recyclerView.setAdapter(homeRecyclerViewHorizontalAdapter);
     }
 }
---
+++ app/src/main/java/com/example/mockupsai/Home/HomeRecyclerViewHorizontalAdapter.java
@@ -0,0 +1,53 @@
+package com.example.mockupsai.Home;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mockupsai.R;
+
+import java.util.List;
+
+public class HomeRecyclerViewHorizontalAdapter extends RecyclerView.Adapter<HomeRecyclerViewHorizontalAdapter.ViewHolder> {
+    private List<Homes> homesList;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_home_horizontal, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Homes homes = homesList.get(position);
+        holder.textTitle.setText((String.valueOf((homes.getTitle()))));
+        holder.textDate.setText((String.valueOf((homes.getDate()))));
+        holder.textTime.setText((String.valueOf((homes.getTime()))));
+    }
+
+    @Override
+    public int getItemCount() {
+        return homesList.size();
+    }
+
+    public HomeRecyclerViewHorizontalAdapter(List<Homes> homesList) {
+        this.homesList = homesList;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView textTitle, textDate, textTime;
+
+        public ViewHolder(@NonNull View v) {
+            super(v);
+            textTitle = (TextView) v.findViewById(R.id.textTitle);
+            textDate = (TextView) v.findViewById(R.id.textDate);
+            textTime = (TextView) v.findViewById(R.id.textTime);
+        }
+    }
+}
