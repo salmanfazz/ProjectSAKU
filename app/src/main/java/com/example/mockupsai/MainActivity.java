0e68318a8a80ce35afd9ee8a6d153a32c9ac8276
devptsai <devptsai@gmail.com>
2019-11-01 08:58:59 +0700

    Revert Change

--- app/src/main/java/com/example/mockupsai/MainActivity.java
+++ app/src/main/java/com/example/mockupsai/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.mockupsai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
